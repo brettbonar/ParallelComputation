701f1538f49ee73f6e38cf1c23f7eba7ea124258
bbonar <brettbonar@gmail.com>
2018-09-21 21:19:14 -0600

    Update

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -19,7 +19,8 @@ int main(int argc, char **argv){
   int length = 64;
   int sortSize = length / size;
 
-  std::vector<int> fullList(length);
+  std::vector<int> sortedList(length);
+  std::vector<int> aggregateList(length);
   std::vector<int> sortList(sortSize);
   if (rank == 0)
   {
@@ -33,13 +34,35 @@ int main(int argc, char **argv){
 
   std::sort(sortList.begin(), sortList.end());
 
-  MPI_Gather(sortList.data(), sortSize, MPI_INT, fullList.data(), sortSize, MPI_INT, 0, MCW);
+  MPI_Gather(sortList.data(), sortSize, MPI_INT, aggregateList.data(), sortSize, MPI_INT, 0, MCW);
 
   if (rank == 0)
   {
+    // Init sorted list with first sublist
+    for (int i = 0; i < sortSize; i++)
+    {
+      sortedList[i] = aggregateList[i];
+    }
+
+    // Aggregate list will contain size sublists of length sortSize
+    for (int i = sortSize; i < length; i += sortSize)
+    {
+      int pos = 0;
+      // Iterate over each item in sublist
+      for (int j = 0; j < sortSize; j++)
+      {
+        while (aggregateList[i + j] > sortedList[pos] && pos < i)
+        {
+          pos++;
+        }
+
+        sortedList.insert(sortedList.begin() + pos, aggregateList[i + j]);
+      }
+    }
+
     for (int i = 0; i < length; i++)
     {
-      std::cerr << fullList[i] << std::endl;
+      std::cerr << sortedList[i] << std::endl;
     }
   }
 
