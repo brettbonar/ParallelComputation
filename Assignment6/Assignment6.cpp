bb05b439f226cc525bb4f9efd0cc150579ab1364
bbonar <brettbonar@gmail.com>
2018-10-05 22:40:13 -0600

    Test

--- Assignment6/Assignment6.cpp
+++ Assignment6/Assignment6.cpp
@@ -110,7 +110,7 @@ int main(int argc, char **argv){
   //cout << "Gather S: " << rank << ", Size: " << localColors.size() << endl;
 
   MPI_Gather(localColors.data(), numLocalColors, MPI_INT,
-    globalColors.data(), numGlobalColors, MPI_INT, 0, MCW);
+    globalColors.data(), numLocalColors, MPI_INT, 0, MCW);
 
   //cout << "Gather D: " << rank << endl;
 
