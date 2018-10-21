aecfa1b1d12b6df74bdc722cdab17a18932fc29c
bbonar <brettbonar@gmail.com>
2018-10-21 13:24:02 -0600

    Fix

--- Assignment7/Assignment7.cpp
+++ Assignment7/Assignment7.cpp
@@ -18,19 +18,22 @@ int main(int argc, char **argv){
   MPI_Comm_size(MCW, &size);
 
   int myValue = std::rand() % MAX;
+
+  std::cout << "Rank: " << rank << ", Value: " << myValue;
+
   int mySum = myValue;
   int recvSum = 0;
   int target = rank + 1 % size;
 
   while (target != rank)
   {
-    MPI_Send(&sum, 1 , MPI_INT, target, 0, MCW);
+    MPI_Send(&mySum, 1 , MPI_INT, target, 0, MCW);
     MPI_Recv(&recvSum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW, MPI_STATUS_IGNORE);
 
     mySum += recvSum;
   }
 
-  std::cout << "Rank: " << rank << ", Sum: " << sum;
+  std::cout << "Rank: " << rank << ", Sum: " << mySum;
   
   MPI_Finalize();
 
