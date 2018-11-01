64bed155bc4faf9af0c0cabf91c889f0cb8999b2
bbonar <brettbonar@gmail.com>
2018-10-31 22:19:47 -0600

    Test

--- Assignment8/Assignment8.cpp
+++ Assignment8/Assignment8.cpp
@@ -142,7 +142,7 @@ void updateWorld(int world[][WORLD_SIZE], int targetWorld[][WORLD_SIZE], int loc
 int main(int argc, char **argv){
   int rank, size;
   int data;
-  int world[WORLD_SIZE][WORLD_SIZE];
+  int** world;
   int iterations = 100;
 -
   MPI_Init(&argc, &argv);
