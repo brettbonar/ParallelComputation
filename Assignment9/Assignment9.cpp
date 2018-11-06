f889714836e813ee428a4b055c6c59759b520ff5
bbonar <brettbonar@gmail.com>
2018-11-05 20:50:40 -0700

    Start

--- Assignment9/Assignment9.cpp
+++ Assignment9/Assignment9.cpp
@@ -7,14 +7,20 @@
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <thread>
 #define MCW MPI_COMM_WORLD
+
+const int TASK_THRESHOLD = 16;
+const int JOB = 0;
 
 int main(int argc, char **argv){
   int rank, size;
   int data;
+  int sendData = 1;
 
-  int jobFlag;
   MPI_Request myRequest;
+  MPI_Request sendRequest;
   MPI_Status myStatus;
 -
   MPI_Init(&argc, &argv);
@@ -26,19 +32,34 @@ int main(int argc, char **argv){
   // Process turns black only if it has sent work to a lower process. Because token can't be aware of lower things
   // or something like that.
 
-  if (rank == 0)
-  {
-    job = 0;
-    job++;
-    MPI_Send(&job, 1, MPI_INT, 0, JOB, MCW);
-    // job++, MPI_Send ...
-  }
+  srand(time(nullptr) * rank);
+  int numTasks = rand() % 32;
 
-  while (1)
+  bool done = false;
+  while (!done)
   {
-    MPI_Irecv(&newJob, 1, MPI_INT, MPI_ANY_SOURCE, JOB, MCW, &myRequest);
-    MPI_Test(&myRequest, &jobFlag, &myStatus);
-    if (!jobFlag) break;
+    if (numTasks > TASK_THRESHOLD)
+    {
+      int count = numTasks - TASK_THRESHOLD;
+      for (int i = 0; i < count; i++)
+      {
+        MPI_Isend(&sendData, 1, MPI_INT, rand() % size, JOB, MCW, &sendRequest);
+      }
+      numTasks -= count;
+    }
+
+    MPI_Irecv(&sendData, 1, MPI_INT, MPI_ANY_SOURCE, JOB, MCW, &myRequest);
+    int jobFlag = 0;
+    while (numTasks && !jobFlag)
+    {
+      // Do work then check for more work
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      MPI_Test(&myRequest, &jobFlag, &myStatus)
+    }
+    if (jobFlag)
+    {
+      numTasks++;
+    }
   }
 
   MPI_Finalize();
